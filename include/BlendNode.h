ab9d4814766982de24d23cff2c1d830824f79814
Piyush-Singh1234 <136171946+Piyush-Singh1234@users.noreply.github.com>
2025-04-10 14:50:05 +0530

    Update BlendNode.h

--- include/BlendNode.h
+++ include/BlendNode.h
@@ -4,12 +4,11 @@
 
 class BlendNode : public Node {
 public:
-    enum class BlendMode { Normal, Multiply, Screen, Overlay, Difference };
-    BlendMode mode;
+    enum class BlendNode { Normal, Multiply, Screen, Overlay, Difference };
+    BlendNode mode;
     float opacity;
 
     explicit BlendNode(int id_);
     void render() override;
     cv::Mat process(const std::vector<cv::Mat>& inputs) override;
 };
-
