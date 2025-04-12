bf8267fe5a8c1fb37c38ac7eb57a16bd288648dc
Piyush-Singh1234 <136171946+Piyush-Singh1234@users.noreply.github.com>
2025-04-13 02:38:09 +0530

    Update BlendNode.h

--- include/BlendNode.h
+++ include/BlendNode.h
@@ -1,37 +1,3 @@
-// #pragma once
-
-// #include "Node.h"
-// #include <opencv2/opencv.hpp>
-// #include <string>
-
-// enum class BlendMode {
-//     Normal = 0,
-//     Multiply,
-//     Screen,
-//     Overlay,
-//     Difference
-// };
-
-// class BlendNode : public Node {
-// public:
-//     BlendMode mode = BlendMode::Normal;
-//     float opacity = 0.5f;  // Opacity/mix value (0.0 to 1.0)
-
-//     explicit BlendNode(int _id);
-
-//     void render() override;
-//     cv::Mat process(const std::vector<cv::Mat>& inputs) override;
-//     bool validateInputs(const std::vector<cv::Mat>& inputs) const override {
-//         return inputs.size() == 2 && !inputs[0].empty() && !inputs[1].empty();
-//     }
-//     std::vector<int> getInputAttributes() const override { 
-//         return { id * 10 + 1, id * 10 + 2 }; 
-//     }
-
-//     // Override to match the output attribute used in render()
-//     int getOutputAttr() const override { return id * 10 + 3; }
-// };
-
 
 #pragma once
 
