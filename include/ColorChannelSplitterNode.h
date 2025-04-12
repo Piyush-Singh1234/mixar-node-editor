9c70fdc6dc92b31cd7281e93f042bcc177d538af
Piyush-Singh1234 <136171946+Piyush-Singh1234@users.noreply.github.com>
2025-04-13 02:38:51 +0530

    Update ColorChannelSplitterNode.h

--- include/ColorChannelSplitterNode.h
+++ include/ColorChannelSplitterNode.h
@@ -1,23 +1,4 @@
 
-// #pragma once
-// #include "Node.h"
-// #include <opencv2/opencv.hpp>
-// #include <vector>
-
-// class ColorChannelSplitterNode : public Node {
-//     public:
-//         explicit ColorChannelSplitterNode(int id_);
-    
-//         void render() override;
-//         cv::Mat process(const std::vector<cv::Mat>& inputs) override;  // required override
-//         std::vector<cv::Mat> getChannelOutputs(const std::vector<cv::Mat>& inputs);
-    
-       
-//     private:
-//         bool outputGrayscale;
-//     };
-
-
 #pragma once
 
 #include "Node.h"
